ff3df281e5c744f824c6ed9f78217d401e6a9691
20062573 <20062573@mydbs.ie>
2025-11-03 10:30:57 +0000

    terraform commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,3 +1,21 @@
 provider "aws"{
     region = "eu-west-1"
 } 
+resource "aws_security_group" "formxpress_sg" {
+  name_prefix = "formxpress-sg-"
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
